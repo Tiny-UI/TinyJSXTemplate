ed3b555980be78f21eb8a4def13ba35f04172d40
Neo <853151446@qq.com>
2022-03-15 20:15:25 +0800

    TinyUI dev tools JSX template project

---
+++ src/page/hello/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const page = () => {
+    return (
+        <column id="container">
+            <text id={"text"} style={{fontSize: 50}}>Hello，Tiny UI!</text>
+        </column>
+    );
+}
+
+TinyUI.render(page());
+
+
+
